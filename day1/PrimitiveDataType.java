6f76bb55e62a0f2650e93910fe5d4357826a48c4
husain09121993 <142678240+husain09121993@users.noreply.github.com>
2023-08-19 20:43:33 +0530

    Add files via upload
    Day1

---
+++ day1/PrimitiveDataType.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class PrimitiveDataType {
+ public static void main(String[] args) {
+	 
+	int rent = 10000;
+	boolean oddNumber = true;
+	String mobileBrand = "vivo";
+	
+	System.out.println("My rent for this month is : " +rent );
+	System.out.println("My Moble Brand is: " +mobileBrand + " And Let me know if 1 a Odd number :" +oddNumber);
+}
+
+}
